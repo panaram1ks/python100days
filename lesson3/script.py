2c50b6014b37e646f25d336a9da3459526fffe18
parom <E.Parominskiy@isida.by>
2026-03-24 12:56:12 +0300

    Section 3: Day 3 - Beginner - Control Flow and Logical Operators 23. Introducing the Modulo

--- lesson3/script.py
+++ lesson3/script.py
@@ -1,9 +1,15 @@
-print("Welcome to the rollercoaster!")
-height = int(input("What is your height in cm? "))
+# print("Welcome to the rollercoaster!")
+# height = int(input("What is your height in cm? "))
+#
+# if height > 120:
+#     print("Your can ride the rollercoaster!")
+#     print("additional line if")
+# else:
+#     print("Sorry, you have to grow taller before you can ride.")
+#     print("additional line else")
 
-if height > 120:
-    print("Your can ride the rollercoaster!")
-    print("additional line if")
+numberInput = int(input("What is the number you want to check?"))
+if numberInput % 2 == 0:
+    print("even")
 else:
-    print("Sorry, you have to grow taller before you can ride.")
-    print("additional line else")
+    print("odd")
