b99db2ce44a66b97bf369cfac372b15a321deee5
parom <E.Parominskiy@isida.by>
2026-03-24 12:44:50 +0300

    Section 3: Day 3 - Beginner - Control Flow and Logical Operators 22. Control Flow with if / else and Conditional Operators

---
+++ lesson3/script.py
@@ -0,0 +1,9 @@
+print("Welcome to the rollercoaster!")
+height = int(input("What is your height in cm? "))
+
+if height > 120:
+    print("Your can ride the rollercoaster!")
+    print("additional line if")
+else:
+    print("Sorry, you have to grow taller before you can ride.")
+    print("additional line else")
