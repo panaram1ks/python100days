b34ed4866549621c705f71f0fcee471d90e90422
parom <E.Parominskiy@isida.by>
2026-03-25 09:42:17 +0300

    Section 3: Day 3 - Beginner - Control Flow and Logical Operators 25. Multiple If Statements in Succession

--- lesson3/nestedif.py
+++ lesson3/nestedif.py
@@ -1,30 +1,41 @@
 print("Welcome to the rollercoaster!")
 height = int(input("What is your height in cm? "))
+bill = 0
 
 if height >= 120:
-    print("Please pay $5.")
+    print("You can ride the rollercoaster!")
     age = int(input("What is your age? "))
     if age <= 12:
-        print("Please pay $5.")
+        bill = 5
+        print(f"Child tickets are ${bill}.")
     elif age <= 18:
-        print("Please pay $7.")
+        bill = 7
+        print(f"Youth ticket are ${bill}.")
     else:
-        print("Please pay $12.")
+        bill = 12
+        print(f"Adult tickets are ${bill}.")
+
+    wants_photo = input("Do you want a photo take? Type y for Yes and n for No.")
+    if wants_photo == "y":
+        bill += 3
+
+    print(f"Your final bill is ${bill}")
+
 else:
     print("Sorry, you have to grow taller before you can ride.")
 
 
 
-weight = 85
-height = 1.85
-
-bmi = weight / (height ** 2)
-
-# 🚨 Do not modify the values above
-# Write your code below 👇
-if bmi < 18.5:
-    print("underweight")
-elif bmi >=18.5 and bmi < 25:
-    print("normal weight")
-elif bmi >= 25:
-    print("overweight")
+# weight = 85
+# height = 1.85
+#
+# bmi = weight / (height ** 2)
+#
+# # 🚨 Do not modify the values above
+# # Write your code below 👇
+# if bmi < 18.5:
+#     print("underweight")
+# elif 18.5 <= bmi < 25:
+#     print("normal weight")
+# elif bmi >= 25:
+#     print("overweight")
